24108af4cd593a4d9f5de1c8b6062e8acb9ced98
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2013-09-01 19:25:21 -0300

    remove the example model

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@
 
 # Core Django imports
 from django.db import models
-from django.utls.translation import ugettext as _
+from django.utils.translation import ugettext as _
 
 
 class TimeStampedModel(models.Model):
@@ -18,36 +18,3 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-
-class Example(TimeStampedModel):
-    """
-    An example class model to standardize
-    new models of project.
-    """
-
-    _STATUS_MAX_LENGTH = 1
-    _STATUS_DEFAULT = 'D'
-    _STATUS_CHOICES = (
-        ('A', _(u'Ativo')),
-        ('B', _(u'Bloqueado')),
-        ('C', _(u'Cancelado')),
-        (_STATUS_DEFAULT, _(u'Em aprovação')),
-        ('E', _(u'Inativo')),
-    )
-
-    title = models.CharField(_(u'Titulo'), max_length=255)
-    slug = models.SlugField(_(u'Slug'), unique=True)
-    status = models.CharField(
-        _(u'Status'),
-        choices=_STATUS_CHOICES,
-        default=_STATUS_DEFAULT,
-        max_length=_STATUS_MAX_LENGTH
-    )
-
-    class Meta:
-        ordering = ['created']
-        verbose_name = _(u'Example')
-        verbose_name_plural = _(u'Examples')
-
-    def __unicode__(self):
-        return u'%s' % (self.title)
